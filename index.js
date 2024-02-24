f19be0e9a36ecb88fa1b1a6e0dba4199a961a5e8
Adrien <adriengoncalves2003@gmail.com>
2024-02-24 09:33:07 +0100

    version 1

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-const words = ["cut", "love", "house", "eat", "boys"];
+const words = [
+  { word: "cut", def: "sdafae" },
+  { word: "love", def: "sdafae" },
+  { word: "house", def: "sdafae" },
+  { word: "eat", def: "sdafae" },
+  { word: "boys", def: "sdafae" },
+];
 
 const start = () => {
   const _words = words.slice();
 
   const elt = document.querySelector(".word");
-  console.log(elt);
-  while (_words.length > 0) {
-    let random = Math.floor(Math.random() * _words.length);
-    elt.innerHTML = _words[random];
-    console.log(_words[random]);
-  }
+  const but = document.querySelector(".button");
+
+  let isClicked = false;
+
+  const displayNextWord = () => {
+    if (_words.length > 0) {
+      if (!isClicked) {
+        let randomIndex = Math.floor(Math.random() * _words.length);
+        elt.innerHTML = `<span class="word-style">${_words[randomIndex].word}</span>`;
+        _words.splice(randomIndex, 1);
+      } else {
+        elt.innerHTML = `<span class="definition-style">${_words[0].def}</span>`;
+        isClicked = false;
+      }
+    } else {
+      elt.innerHTML = "Fin des mots";
+    }
+  };
+
+  displayNextWord();
+
+  elt.addEventListener("click", () => {
+    isClicked = false;
+    console.log(isClicked);
+    displayNextWord();
+  });
+
+  but.addEventListener("click", () => {
+    isClicked = true;
+    console.log(isClicked);
+    displayNextWord();
+  });
 };
 
 start();
